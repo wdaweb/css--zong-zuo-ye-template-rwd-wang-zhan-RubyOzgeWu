dbc407a55b124b98f9a0c6f1be22a72f67726b5b
RubyOzgeWu <rubyozgewu@gmail.com>
2022-07-01 15:28:13 +0800

    section05 功能

---
+++ index.js
@@ -0,0 +1,79 @@
+
+AOS.init({
+    duration: 1000
+});
+
+
+
+
+/* section01----------------------------------------------------- */
+
+var sec01Swiper = new Swiper("#section01 .mySwiper", {
+    spaceBetween: 50,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+var sec05Swiper = new Swiper("#section05 .mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    }
+})
+
+
+// $('.swiper-pagination-bullet').on('click', function () {
+//     $('.carousel-caption').addClass('animated')
+//     if ($('.carousel-caption').hasClass('animated') == true) {
+//         $('.swiper-pagination-bullet').removeClass('animated')
+//     }
+// })
+
+/* section05--------------------------------------------- */
+
+$(document).ready(function(){
+    $('.collapse1').hide()
+    $('.accordion-btn1').click(function(){
+        $('.collapse1').slideToggle(500)
+        $('.collapse2').slideUp(500)
+        $('.collapse3').slideUp(500)
+        return false
+    })
+})
+$(document).ready(function(){
+    $('.collapse2').hide()
+    $('.accordion-btn2').click(function(){
+        $('.collapse2').slideToggle(500)
+        $('.collapse1').slideUp(500)
+        $('.collapse3').slideUp(500)
+        return false
+    })
+})
+$(document).ready(function(){
+    $('.collapse3').hide()
+    $('.accordion-btn3').click(function(){
+        $('.collapse3').slideToggle(500)
+        $('.collapse2').slideUp(500)
+        $('.collapse1').slideUp(500)
+        return false
+    })
+})
+
+$(document).ready(function(){
+    $('.open').click(function(){
+        $(this).addClass('active')
+        $('.close').addClass('active')
+    })
+    $('.close').click(function(){
+        $(this).removeClass('active')
+        $('.open').removeClass('active')
+    })
+})
