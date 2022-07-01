d8245a03657d83f03dc141c3038a3a5130f43edd
RubyOzgeWu <rubyozgewu@gmail.com>
2022-07-01 16:23:44 +0800

    section06 功能

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ AOS.init({
 
 
 
-/* section01----------------------------------------------------- */
+/* section01-------------------------------------------------------------------------*/
 
 var sec01Swiper = new Swiper("#section01 .mySwiper", {
     spaceBetween: 50,
@@ -16,6 +16,14 @@ var sec01Swiper = new Swiper("#section01 .mySwiper", {
     },
 });
 
+// $('.swiper-pagination-bullet').on('click', function () {
+//     $('.carousel-caption').addClass('animated')
+//     if ($('.carousel-caption').hasClass('animated') == true) {
+//         $('.swiper-pagination-bullet').removeClass('animated')
+//     }
+// })
+
+/* section05------------------------------------------------------------------------ */
 var sec05Swiper = new Swiper("#section05 .mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
@@ -29,37 +37,27 @@ var sec05Swiper = new Swiper("#section05 .mySwiper", {
     }
 })
 
-
-// $('.swiper-pagination-bullet').on('click', function () {
-//     $('.carousel-caption').addClass('animated')
-//     if ($('.carousel-caption').hasClass('animated') == true) {
-//         $('.swiper-pagination-bullet').removeClass('animated')
-//     }
-// })
-
-/* section05--------------------------------------------- */
-
-$(document).ready(function(){
+$(document).ready(function () {
     $('.collapse1').hide()
-    $('.accordion-btn1').click(function(){
+    $('.accordion-btn1').click(function () {
         $('.collapse1').slideToggle(500)
         $('.collapse2').slideUp(500)
         $('.collapse3').slideUp(500)
         return false
     })
 })
-$(document).ready(function(){
+$(document).ready(function () {
     $('.collapse2').hide()
-    $('.accordion-btn2').click(function(){
+    $('.accordion-btn2').click(function () {
         $('.collapse2').slideToggle(500)
         $('.collapse1').slideUp(500)
         $('.collapse3').slideUp(500)
         return false
     })
 })
-$(document).ready(function(){
+$(document).ready(function () {
     $('.collapse3').hide()
-    $('.accordion-btn3').click(function(){
+    $('.accordion-btn3').click(function () {
         $('.collapse3').slideToggle(500)
         $('.collapse2').slideUp(500)
         $('.collapse1').slideUp(500)
@@ -67,13 +65,31 @@ $(document).ready(function(){
     })
 })
 
-$(document).ready(function(){
-    $('.open').click(function(){
+$(document).ready(function () {
+    $('.open').click(function () {
         $(this).addClass('active')
         $('.close').addClass('active')
     })
-    $('.close').click(function(){
+    $('.close').click(function () {
         $(this).removeClass('active')
         $('.open').removeClass('active')
     })
+})
+
+/* section06------------------------------------------------------------------------------- */
+var sec06Swiper = new Swiper("#section06 .mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 })
