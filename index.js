7ebd4a0be0ec98b4f8371c5948d81ce89c9903d3
Ruby Oz <rubyozgewu@gmail.com>
2022-07-17 15:31:39 +0800

    finished

--- index.js
+++ index.js
@@ -3,6 +3,97 @@ AOS.init({
     duration: 1000
 });
 
+/* navbar---------------------------------------- */
+// hamburger //
+$('.nav-toggler').click(function(){
+    if($('.menu').hasClass('show')){
+        $('.menu').removeClass('show')
+        $('.fa-x').css('display','none')
+        $('.fa-bars').css('display','block')
+    }else{
+        $('.menu').addClass('show')
+        $('.fa-bars').css('display','none')
+        $('.fa-x').css('display','block')
+
+    }
+})
+
+
+// menu //
+$(document).ready(function(){
+    $('.pages-dropdown').hide()
+    $('.pages').click(function(){
+        $('.pages-dropdown').slideToggle(500)
+        $('.blog-dropdown').slideUp(500)
+        $('.shop-dropdown').slideUp(500)
+
+        if($('.i-toggler-1').hasClass('rotate')){
+            $('.i-toggler-1').removeClass('rotate')
+        }else{
+            $('.i-toggler-1').addClass('rotate')
+        }
+    })
+})
+$(document).ready(function(){
+    $('.blog-dropdown').hide()
+    $('.blog').click(function(){
+        $('.blog-dropdown').slideToggle(500)
+        $('.pages-dropdown').slideUp(500)
+        $('.shop-dropdown').slideUp(500)
+
+        if($('.i-toggler-2').hasClass('rotate')){
+            $('.i-toggler-2').removeClass('rotate')
+        }else{
+            $('.i-toggler-2').addClass('rotate')
+        }
+    })
+})
+$(document).ready(function(){
+    $('.shop-dropdown').hide()
+    $('.shop').click(function(){
+        $('.shop-dropdown').slideToggle(500)
+        $('.blog-dropdown').slideUp(500)
+        $('.pages-dropdown').slideUp(500)
+
+        if($('.i-toggler-3').hasClass('rotate')){
+            $('.i-toggler-3').removeClass('rotate')
+        }else{
+            $('.i-toggler-3').addClass('rotate')
+        }
+    })
+})
+
+// search //
+$('.search').click(function(){
+    if($('.navbar-hide').hasClass('show')){
+        $('.navbar-hide').removeClass('show')
+    }else{
+        $('.navbar-hide').addClass('show')
+    }
+
+})
+
+// lang //
+$('.lang').click(function(){
+    if($('.lang-dropdown').hasClass('show')){
+        $('.lang-dropdown').removeClass('show')
+    }else{
+        $('.lang-dropdown').addClass('show')
+    }
+
+})
+
+// popover //
+$('.popover').click(function(){
+    if($('.popover-info').hasClass('show')){
+        $('.popover-info').removeClass('show')
+    }else{
+        $('.popover-info').addClass('show')
+    }
+
+})
+
+
 
 
 
@@ -14,14 +105,29 @@ var sec01Swiper = new Swiper("#section01 .mySwiper", {
         el: ".swiper-pagination",
         clickable: true,
     },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
 
-// $('.swiper-pagination-bullet').on('click', function () {
-//     $('.carousel-caption').addClass('animated')
-//     if ($('.carousel-caption').hasClass('animated') == true) {
-//         $('.swiper-pagination-bullet').removeClass('animated')
-//     }
-// })
+/* section04------------------------------------------------------------------------ */
+let valueDisplays=document.querySelectorAll('.num')
+let interval=2000
+
+valueDisplays.forEach((valueDisplay)=>{
+    let startValue=0
+    let endValue=parseInt(valueDisplay.getAttribute('data-val'))
+    let duration=Math.floor(interval/endValue)
+    let counter=setInterval(function(){
+        startValue+=1
+        valueDisplay.textContent=startValue
+        if(startValue==endValue){
+            clearInterval(counter)
+        }
+    },duration)
+})
+
 
 /* section05------------------------------------------------------------------------ */
 var sec05Swiper = new Swiper("#section05 .mySwiper", {
@@ -76,6 +182,7 @@ $(document).ready(function () {
     })
 })
 
+
 /* section06------------------------------------------------------------------------------- */
 var sec06Swiper = new Swiper("#section06 .mySwiper", {
     spaceBetween: 30,
@@ -93,3 +200,84 @@ var sec06Swiper = new Swiper("#section06 .mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 })
+
+/* section07------------------------------------------------------------------------------------- */
+$('.select').click(function(){
+    if($('.caret').hasClass('caret-rotate')){
+        $('.caret').removeClass('caret-rotate')
+        $('.menu').removeClass('menu-open')
+    } else{
+        $('.caret').addClass('caret-rotate')
+        $('.menu').addClass('menu-open')
+    }
+})
+
+/* section 09----------------------------------------------------------- */
+// var sec09Swiper = new Swiper("#section09 .mySwiper", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     }
+// })
+
+var sec09Swiper = new Swiper("#section09 .mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        768:{
+            slidesPerView: 2,
+        },
+        1200:{
+            slidesPerView: 4,
+
+        }
+    }
+});
+
+/* section10--------------------------------------------------------------- */
+var sec10Swiper = new Swiper("#section10 .mySwiper", {
+    spaceBetween: 30,
+    slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        768:{
+            slidesPerView:2
+        },
+        1200:{
+            slidesPerView:3
+        }
+    },
+
+});
+
+/* section11---------------------------------------------------------------------- */
+var sec11Swiper = new Swiper("#section11 .mySwiper", {
+    spaceBetween: 30,
+    // centeredSlides: true,
+    slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        768:{
+            slidesPerView: 2,
+        },
+        1200:{
+            slidesPerView:3
+        }
+    }
+})
+
+
+/* RWD */
+/* 768px-------------------------------------------------------- */
